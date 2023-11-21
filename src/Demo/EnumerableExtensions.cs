98ec7009e1701cb73e6d0d042df019efd4f1d37b
Christof <christof_sprenger@hotmail.com>
2023-11-20 16:05:03 -0800

    usings

--- src/Demo/EnumerableExtensions.cs
+++ src/Demo/EnumerableExtensions.cs
@@ -1,5 +1,4 @@
 
-using System.Diagnostics;
 
 static class EnumerableExtensions
 {
--- src/Demo/GlobalsUsings.cs
+++ src/Demo/GlobalsUsings.cs
@@ -1,5 +1,5 @@
 
 
-global using System.Collections.Immutable;
 global using System.Linq.Expressions;
 global using System.Reflection;
+global using System.Diagnostics;
